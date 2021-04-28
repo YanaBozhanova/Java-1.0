be43047cfbb3a318ee81f62aa303db20b52663cc
Yana <74353738+YanaBozhanova@users.noreply.github.com>
2021-04-28 13:48:03 +0300

    Добавила решение Задачи №11_Работа с данными

---
+++ src/main/java/Task11.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        System.out.println("Введите два числа:");
+
+    Scanner console = new Scanner(System.in);
+
+    String s = console.nextLine();
+    //String s = Integer.toString(x);
+
+    int y = console.nextInt();
+
+    int x1 = Integer.parseInt(s);
+
+    int resultmax = Math.max(y,  x1);
+        System.out.println(resultmax);
+
+       /* Вариант с введением дополнтельной переменной для конвертации в double 
+        int  resultmin = Math.min(y,  x1);
+        double f = resultmin;
+        System.out.println(f);*/
+        double resultmin = Math.min(y,  x1);
+        System.out.println(resultmin);
+
+    }
+}
